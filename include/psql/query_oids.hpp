ac83b052d50596ee65c7a996c85b4de3a1c925d5
Mohammad <ashtumashtum@gmail.com>
2023-08-15 15:05:11 +0000

    Make async_query_oids a composed operation

--- include/psql/query_oids.hpp
+++ include/psql/query_oids.hpp
@@ -1,5 +1,4 @@
 #include <psql/connection.hpp>
-#include <psql/detail/deserialization.hpp>
 
 namespace psql
 {
@@ -8,29 +7,37 @@ auto async_query_oids(
   oid_map& omp,
   asio::completion_token_for<void(boost::system::error_code)> auto&& token)
 {
-  return asio::async_initiate<decltype(token), void(boost::system::error_code)>(
-    asio::experimental::co_composed<void(boost::system::error_code)>(
-      [](auto state, connection* conn, oid_map* omp) -> void
+  return asio::async_compose<decltype(token), void(boost::system::error_code)>(
+    [coro = asio::coroutine{}, conn = &conn, omp = &omp](
+      auto& self, boost::system::error_code ec = {}, result result = {}) mutable
+    {
+      if (ec)
+        return self.complete(ec);
+
+      BOOST_ASIO_CORO_REENTER(coro)
       {
-        state.on_cancellation_complete_with(asio::error::operation_aborted);
+        BOOST_ASIO_CORO_YIELD conn->async_query(
+          R"(
+            SELECT
+              type_name,
+              COALESCE(to_regtype(type_name)::oid, - 1),
+              COALESCE(to_regtype(type_name || '[]')::oid, - 1) 
+            FROM
+              UNNEST($1) As type_name
+          )",
+          omp->get_type_names(),
+          std::move(self));
 
-        const auto type_names   = omp->get_type_names();
-        const auto [ec, result] = co_await conn->async_query(
-          "SELECT t, COALESCE(to_regtype(t)::oid, -1), COALESCE(to_regtype(t || '[]')::oid, -1) FROM UNNEST($1) As t",
-          type_names,
-          asio::as_tuple(asio::deferred));
-
-        for (const auto row : result)
+        // TODO handle exceptions
+        for (auto row : result)
         {
-          const auto [name, type_oid, array_oid] = as<std::string_view, uint32_t, uint32_t>(row);
+          auto [name, type_oid, array_oid] = as<std::string_view, uint32_t, uint32_t>(row);
           omp->set_type_oids(name, type_oid, array_oid);
         }
 
-        co_return ec;
-      },
-      conn.get_executor()),
-    token,
-    &conn,
-    &omp);
+        self.complete({});
+      }
+    },
+    token);
 }
 } // namespace psql
