3502592c8c04b0bd8bc5daca2edd594e42510b44
Mohammad <ashtumashtum@gmail.com>
2023-08-29 09:33:40 +0000

    Get connection-uri for examples from input args

--- example/main.cpp
+++ example/main.cpp
@@ -8,16 +8,23 @@ namespace asio = boost::asio;
 
 asio::awaitable<void> async_main(std::string conninfo);
 
-int main()
+int main(int argc, char** argv)
 {
+  if (argc != 2)
+  {
+    std::cerr << "Usage: ./executable <connection-uri>\n";
+    std::cerr << "Example:\n";
+    std::cerr << "    ./pipeline postgresql://user:secret@localhost:5433\n";
+    return EXIT_FAILURE;
+  }
+
   try
   {
-    auto ioc      = asio::io_context{};
-    auto conninfo = std::string{ "postgresql://postgres:postgres@172.18.0.2:5432" };
+    auto ioc = asio::io_context{};
 
     asio::co_spawn(
       ioc,
-      async_main(conninfo),
+      async_main(argv[1]),
       [](const std::exception_ptr& ep)
       {
         if (ep)
